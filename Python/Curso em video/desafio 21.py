9eabac54b710326a17508a7674dd708f01fef2b5
Marcelo Silva <marcelo.silva070899@gmail.com>
2022-03-20 18:25:04 -0300

    arrumando detalhes
    arrumando alguns detalhes nos codigos

--- Python/Curso em video/desafio 21.py
+++ Python/Curso em video/desafio 21.py
@@ -1,8 +1,11 @@
-import pygame
+import playsound
+
+playsound.playsound('nofun.mp3')
+
+'''import pygame
 pygame.init()
 pygame.mixer.music.load('nofun.mp3')
 pygame.mixer.music.play()
 input()
-pygame.event.wait()
-
+pygame.event.wait()'''
 
